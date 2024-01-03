1317f974a652330a86340780d035d5da9e2f0a79
YASSINE <contact@yassine.fun>
2024-01-02 17:04:18 -0800

    Task 13 - Fix

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-
 def magic_calculation(a, b):
-    """Returns the result of adding 98 to the power of a raised to b."""
-    return (98 + (a ** b))
-
-
-
+    return 98 + a ** b
