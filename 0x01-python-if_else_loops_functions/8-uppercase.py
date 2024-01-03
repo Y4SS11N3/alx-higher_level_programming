522a30b90743d34d5a3776f76ac2f3cfb32206fa
YASSINE <contact@yassine.fun>
2024-01-02 18:24:16 -0800

    Task 8 - Fix

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def uppercase(input_string):
-    
+    output_string = ""
     for char in input_string:
         if ord(char) >= 97 and ord(char) <= 122:
             char = chr(ord(char) - 32)
-            print("{}".format(char), end="")
-            print("")
+        output_string += char
+    print("{}".format(output_string))
