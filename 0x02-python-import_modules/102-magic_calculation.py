6481d6c5a1c133b6d0d89b6ed8a5a7da6765aee8
YASSINE <contact@yassine.fun>
2024-01-04 09:50:13 -0800

    Task 8 - Fix

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/pyhton3
-from magic_calculation_102 import add, sub
-
 def magic_calculation(a, b):
+
+    from magic_calculation_102 import add, sub
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
