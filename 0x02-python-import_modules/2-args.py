7d1e7bdfc9b9b22f6d5be972fa1f0ae36f3ee079
YASSINE <contact@yassine.fun>
2024-01-04 08:54:17 -0800

    Task 2 - Fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-
     import sys
 
     cnt = len(sys.argv) - 1
-    if cnt ==  0:
+    if cnt == 0:
         print("0 arguments.")
     elif cnt == 1:
-        print("1 argument")
+        print("1 argument:")
     else:
         print("{} arguments:".format(cnt))
     for c in range(cnt):
-        print("{}: {}".format(c +1, sys.argv[c +1]))
+        print("{}: {}".format(c + 1, sys.argv[c + 1]))
 
