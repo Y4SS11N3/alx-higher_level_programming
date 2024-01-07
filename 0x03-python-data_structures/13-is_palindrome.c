ef8ea6523bbec56c01d62fbb051e4c5333c4113b
YASSINE <contact@yassine.fun>
2024-01-06 17:55:18 -0800

    Task 13 - Fix

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -8,16 +8,17 @@
  */
 listint_t *reverse_list(listint_t **head)
 {
-    listint_t *prev = NULL, *current = *head, *next = NULL;
-    while (current != NULL)
-    {
-        next = current->next;
-        current->next = prev;
-        prev = current;
-        current = next;
-    }
-    *head = prev;
-    return (*head);
+	listint_t *prev = NULL, *current = *head, *next = NULL;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+	*head = prev;
+	return (*head);
 }
 
 /**
@@ -29,42 +30,42 @@ listint_t *reverse_list(listint_t **head)
  */
 int is_palindrome(listint_t **head)
 {
-    listint_t *slow = *head, *fast = *head, *prev_slow = *head;
-    listint_t *second_half, *middle = NULL;
-    int res = 1;
+	listint_t *slow = *head, *fast = *head, *prev_slow = *head;
+	listint_t *second_half, *middle = NULL;
+	int res = 1;
 
-    if (*head != NULL && (*head)->next != NULL)
-    {
-        while (fast != NULL && fast->next != NULL)
-        {
-            fast = fast->next->next;
-            prev_slow = slow;
-            slow = slow->next;
-        }
+	if (*head != NULL && (*head)->next != NULL)
+	{
+		while (fast != NULL && fast->next != NULL)
+		{
+			fast = fast->next->next;
+			prev_slow = slow;
+			slow = slow->next;
+		}
 
-        if (fast != NULL)
-        {
-            middle = slow;
-            slow = slow->next;
-        }
+		if (fast != NULL)
+		{
+			middle = slow;
+			slow = slow->next;
+		}
 
-        second_half = slow;
-        prev_slow->next = NULL;
-        reverse_list(&second_half);
+		second_half = slow;
+		prev_slow->next = NULL;
+		reverse_list(&second_half);
 
-        res = compare_lists(*head, second_half);
+		res = compare_lists(*head, second_half);
 
-        reverse_list(&second_half);
+		reverse_list(&second_half);
 
-        if (middle != NULL)
-        {
-            prev_slow->next = middle;
-            middle->next = second_half;
-        }
-        else
-            prev_slow->next = second_half;
-    }
-    return (res);
+		if (middle != NULL)
+		{
+			prev_slow->next = middle;
+			middle->next = second_half;
+		}
+		else
+			prev_slow->next = second_half;
+	}
+	return (res);
 }
 
 /**
@@ -76,22 +77,22 @@ int is_palindrome(listint_t **head)
  */
 int compare_lists(listint_t *head1, listint_t *head2)
 {
-    listint_t *temp1 = head1;
-    listint_t *temp2 = head2;
+	listint_t *temp1 = head1;
+	listint_t *temp2 = head2;
 
-    while (temp1 && temp2)
-    {
-        if (temp1->n == temp2->n)
-        {
-            temp1 = temp1->next;
-            temp2 = temp2->next;
-        }
-        else
-            return (0);
-    }
+	while (temp1 && temp2)
+	{
+		if (temp1->n == temp2->n)
+		{
+			temp1 = temp1->next;
+			temp2 = temp2->next;
+		}
+		else
+			return (0);
+	}
 
-    if (temp1 == NULL && temp2 == NULL)
-        return (1);
+	if (temp1 == NULL && temp2 == NULL)
+		return (1);
 
-    return (0);
+	return (0);
 }
