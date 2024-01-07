f500cdc1457b4a29e1bca231c1467b286fe92af4
YASSINE <contact@yassine.fun>
2024-01-06 17:44:43 -0800

    Task 11 - Fix

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     if 0 <= idx < len(my_list):
-        for i in range(idx, len(my_list) - 1):
-            my_list[i] = my_list[i + 1]
-        my_list.pop()
+        del my_list[idx]
     return my_list
