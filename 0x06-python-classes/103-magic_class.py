cdca1105d9b1e743fd65ff12aee66010b09fe8bf
YASSINE <contact@yassine.fun>
2024-01-23 09:18:49 -0800

    Task 10 - Fix

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
+"""MagicClass replicating a specific bytecode structure."""
+
+
 import math
 
-
 class MagicClass:
-    """Represents a circle with methods for
-    calculating its area and circumference."""
+    """Circle representation for area and circumference calculations."""
 
     def __init__(self, radius=0):
-        """Initializes the circle with a given radius."""
-        if type(radius) is not int and type(radius) is not float:
+        """Initializes the circle."""
+        self.__radius = 0
+        if type(radius) not in [int, float]:
             raise TypeError("radius must be a number")
         self.__radius = radius
 
     def area(self):
-        """Calculates the area of the circle."""
+        """Returns the area of the circle."""
         return self.__radius ** 2 * math.pi
 
     def circumference(self):
-        """Calculates the circumference of the circle."""
+        """Returns the circumference of the circle."""
         return 2 * math.pi * self.__radius
