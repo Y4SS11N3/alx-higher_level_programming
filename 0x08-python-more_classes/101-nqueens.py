174c55850e5bb2447c48145fa9fe571b49f65659
YASSINE <contact@yassine.fun>
2024-01-29 08:32:06 -0800

    N queens

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+"""
+Solves the N queens problem.
+"""
+import sys
+
+
+def print_solution(board):
+    """Prints a single board solution."""
+    solution = []
+    for i in range(len(board)):
+        for j in range(len(board)):
+            if board[i][j] == 1:
+                solution.append([i, j])
+    print(solution)
+
+
+def is_safe(board, row, col):
+    """Checks if a queen can be placed at board[row][col]."""
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    for i, j in zip(range(row, len(board), 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    return True
+
+
+def solve_nqueens(board, col):
+    """Uses backtracking to find all solutions."""
+    if col >= len(board):
+        print_solution(board)
+        return
+
+    for i in range(len(board)):
+        if is_safe(board, i, col):
+            board[i][col] = 1
+            solve_nqueens(board, col + 1)
+            board[i][col] = 0
+
+
+def nqueens(n):
+    """Initializes the board and starts the backtracking algorithm."""
+    board = [[0 for _ in range(n)] for _ in range(n)]
+    solve_nqueens(board, 0)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    nqueens(n)
