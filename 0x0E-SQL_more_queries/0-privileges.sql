20f2a4e7d02a2cd3b811f21e5824e6192130fe53
YASSINE <contact@yassine.fun>
2024-03-13 10:53:19 -0700

    My privileges!

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,2 +1,5 @@
--- Lists all privileges of specific MySQL users on your server
-SELECT * FROM information_schema.user_privileges WHERE GRANTEE LIKE '\'user_0d_1\'@\'localhost\'' OR GRANTEE LIKE '\'user_0d_2\'@\'localhost\'';
+-- List privileges for user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- List privileges for user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
