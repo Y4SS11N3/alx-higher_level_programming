08cc6c2a4e86dde00fb5529d1a9e29e66510d23c
YASSINE <contact@yassine.fun>
2024-04-10 19:45:07 -0700

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -9,6 +9,7 @@ from sqlalchemy.ext.declarative import declarative_base
 mymetadata = MetaData()
 Base = declarative_base(metadata=mymetadata)
 
+
 class State(Base):
     """
     Represents a state with an id and name.
