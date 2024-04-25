e306273f9be70569add723b487b8603b857d7b56
YASSINE <contact@yassine.fun>
2024-04-25 14:10:56 -0700

    0x10-python-network_0

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -21,10 +21,14 @@ def find_peak(list_of_integers):
     if length == 2:
         return max(list_of_integers)
 
-    mid = length // 2
-    if list_of_integers[mid] > list_of_integers[mid - 1] and \
-       list_of_integers[mid] > list_of_integers[mid + 1]:
-        return list_of_integers[mid]
-    if list_of_integers[mid] < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
-    return find_peak(list_of_integers[mid + 1:])
+    left = 0
+    right = length - 1
+
+    while left < right:
+        mid = (left + right) // 2
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            left = mid + 1
+        else:
+            right = mid
+
+    return list_of_integers[left]
