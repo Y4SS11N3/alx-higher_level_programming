95b4644e140222376b54dc2f08ffda904a9cccfb
YASSINE <contact@yassine.fun>
2024-04-26 05:46:38 -0700

    0x11-python-network_1

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    query = {"q": letter}
+
+    response = requests.post("http://0.0.0.0:5000/search_user", data=query)
+    try:
+        json_response = response.json()
+        if json_response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(json_response.get("id"), json_response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
