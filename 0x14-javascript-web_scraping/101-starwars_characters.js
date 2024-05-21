3f27cf6c5c9450d9ce6e497eeffc4d0ea3467015
YASSINE <contact@yassine.fun>
2024-05-21 04:17:10 -0700

    Task 8: Print Star Wars movie characters in order

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,18 +1,13 @@
 #!/usr/bin/node
-
 const request = require('request');
-
 const movieId = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
-
 request.get(url, (err, response, body) => {
   if (err) {
     console.error(err);
     return;
   }
-
   const characterUrls = JSON.parse(body).characters;
-
   const characterPromises = characterUrls.map((characterUrl) => {
     return new Promise((resolve, reject) => {
       request.get(characterUrl, (err, response, body) => {
@@ -24,7 +19,6 @@ request.get(url, (err, response, body) => {
       });
     });
   });
-
   Promise.all(characterPromises)
     .then((characters) => {
       characters.forEach((character) => {
