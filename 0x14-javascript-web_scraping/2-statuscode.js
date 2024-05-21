93a05b92f2a5aacdfed32f3253effffc589c12ce
YASSINE <contact@yassine.fun>
2024-05-21 03:24:44 -0700

    Task 2: Display status code of GET request

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const request = require('request');
+
+const url = process.argv[2];
+
+request.get(url, (err, response) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(`code: ${response.statusCode}`);
+  }
+});
