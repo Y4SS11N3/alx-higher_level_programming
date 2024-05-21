45b30f6a0aea25d1d00b41a768e42a2ef8e62669
YASSINE <contact@yassine.fun>
2024-05-21 04:15:58 -0700

    Task 5: Get webpage content and store in file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
-
 const request = require('request');
 const fs = require('fs');
-
 const url = process.argv[2];
 const filePath = process.argv[3];
-
 request.get(url, (err, response, body) => {
   if (err) {
     console.error(err);
