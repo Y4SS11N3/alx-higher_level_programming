b4af9a2023db2fa0f95970997c731beb8e289186
YASSINE <contact@yassine.fun>
2024-05-21 04:15:11 -0700

    Task 3: Print Star Wars movie title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
-
 const movieId = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
-
 request.get(url, (err, response, body) => {
   if (err) {
     console.error(err);
