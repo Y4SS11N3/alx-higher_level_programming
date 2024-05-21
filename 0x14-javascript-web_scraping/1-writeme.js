b671ed10e308563a94f3f0cbf8824a246779bd2c
YASSINE <contact@yassine.fun>
2024-05-21 04:14:33 -0700

    Task 1: Write string to file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 const fs = require('fs');
-
 const filePath = process.argv[2];
 const content = process.argv[3];
-
 fs.writeFile(filePath, content, 'utf-8', (err) => {
   if (err) {
     console.error(err);
