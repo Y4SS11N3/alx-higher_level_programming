e45f0df943aa3da7eba99d6e3bab9d26e9a35c8c
YASSINE <contact@yassine.fun>
2024-05-21 03:26:51 -0700

    Task 6: Compute number of completed tasks by user

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = process.argv[2];
+
+request.get(url, (err, response, body) => {
+  if (err) {
+    console.error(err);
+  } else {
+    const todos = JSON.parse(body);
+    const completedTasks = {};
+
+    todos.forEach((todo) => {
+      if (todo.completed) {
+        if (completedTasks[todo.userId]) {
+          completedTasks[todo.userId]++;
+        } else {
+          completedTasks[todo.userId] = 1;
+        }
+      }
+    });
+
+    console.log(completedTasks);
+  }
+});
