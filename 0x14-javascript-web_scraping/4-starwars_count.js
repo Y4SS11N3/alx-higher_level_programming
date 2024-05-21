f2c6d45e89991a6e9c9a2575fc89442d79d59d16
YASSINE <contact@yassine.fun>
2024-05-21 04:15:35 -0700

    Task 4: Count movies with Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,16 +1,12 @@
 #!/usr/bin/node
-
 const request = require('request');
-
 const url = process.argv[2];
-
 request.get(url, (err, response, body) => {
   if (err) {
     console.error(err);
   } else {
     const films = JSON.parse(body).results;
     let count = 0;
-
     films.forEach((film) => {
       film.characters.forEach((characterUrl) => {
         if (characterUrl.endsWith('/18/')) {
@@ -18,7 +14,6 @@ request.get(url, (err, response, body) => {
         }
       });
     });
-
     console.log(count);
   }
 });
