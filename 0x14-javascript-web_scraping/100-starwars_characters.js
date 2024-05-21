b0e580c89b73e44f591aef3a529dd7851b52fdf3
YASSINE <contact@yassine.fun>
2024-05-21 03:27:26 -0700

    Task 7: Print characters of a Star Wars movie

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request.get(url, async (err, response, body) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  const characters = JSON.parse(body).characters;
+
+  for (const characterUrl of characters) {
+    await new Promise((resolve, reject) => {
+      request.get(characterUrl, (err, response, body) => {
+        if (err) {
+          console.error(err);
+          reject(err);
+        } else {
+          console.log(JSON.parse(body).name);
+          resolve();
+        }
+      });
+    });
+  }
+});
